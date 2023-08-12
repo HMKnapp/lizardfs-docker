a3004357e8262e6e59699aa7a03e8882906b11e7
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2023-08-12 19:49:09 +0200

    Use configurable local mount root
    this is the directory where all disks to be used are mounted

--- docker/scripts/env-to-config.sh
+++ docker/scripts/env-to-config.sh
@@ -72,10 +72,10 @@ if [ -n "$disk_paths" ]; then
     
     # Write each disk path to mfshdd.cfg
     for dir in "${disk_paths[@]}"; do
-        if [ ! -d "/mnt/lizardfs/disks/$dir" ]; then
+        if [ ! -d "$LOCAL_MOUNT_ROOT/$dir" ]; then
             echo "Warning: $LOCAL_MOUNT_ROOT/$dir does not exist on host machine!"
             sleep 10
         fi
-        echo "/mnt/lizardfs/disks/$dir" >> "$file"
+        echo "$LOCAL_MOUNT_ROOT/$dir" >> "$file"
     done
 fi
